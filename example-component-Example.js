f8360b77e353306a9c0cee03d035955aa8f83277
aljun <aljunma20@gmail.com>
2018-08-02 14:35:11 +0800

    Create example-component-Example.js

---
+++ example-component-Example.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example = () => (
+  <div>
+    <p className='text-center'>I'm Working here.... Thank you for wathin......</p>
+  </div>
+);
+
+export default Example;
